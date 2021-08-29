8dbae070fbaf136316e876a6c49a911d9e0ba912
Otto Beyer <oerbeyer@gmail.com>
2021-08-29 17:59:09 +0000

    Add and test JS calculation functions. Test using console.log to output results for controlled group of inputs. Initial test fails revealed syntax errors in math power syntax use ** and not ^ and return arrays syntax use [,] and not just ,. Fixes implemented, all tests pass.

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,7 +26,126 @@ function JQueryTest(target) {
     target.append('<span>JQuery test pass</span>');
 }
 
+/**
+ * Calculates average FGU loss severity.
+ * @param {Number} sigma Pareto Type I scale parameter.
+ * @param {Number} alpha Pareto Type I shape parameter.
+ * @param {Number} cap FGU severity cap (millions).
+ * @returns {Number} Single value (millions).
+ */
+function CappedSeverity(sigma, alpha, cap) {
+    return 1 / ( 1 - alpha ) * ( cap ** ( 1 - alpha ) - sigma ** ( 1 - alpha ) );
+}
 
+/**
+ * Calculates average layer loss severity.
+ * @param {Number} sigma Pareto Type I scale parameter.
+ * @param {Number} alpha Pareto Type I shape parameter.
+ * @param {Number} limit Layer Limit (millions).
+ * @param {Number} excess Layer Excess FGU (millions).
+ * @returns {Number} Single value (millions).
+ */
+function LayerSeverity(sigma, alpha, limit, excess) {
+    let cappedSeverityLimitPlusExcess = CappedSeverity(sigma, alpha, limit + excess);
+    let cappedSeverityExcess = CappedSeverity(sigma, alpha, excess);
+    return cappedSeverityLimitPlusExcess - cappedSeverityExcess;
+}
 
+/**
+ * Calculates Layer Severity Layer 2 / Layer Severity Layer 1 ratio.
+ * @param {Number} sigma Pareto Type I scale parameter.
+ * @param {Number} alpha Pareto Type I shape parameter.
+ * @param {Number} limit1 Layer 1 Limit (millions).
+ * @param {Number} excess1 Layer 1 Excess FGU (millions).
+ * @param {Number} limit2 Layer 2 Limit (millions).
+ * @param {Number} excess2 Layer 2 Excess FGU (millions).
+ * @returns {Number} Single value (percent).
+ */
+function IlfFactor(sigma, alpha, limit1, excess1, limit2, excess2) {
+    let layerSeverity1 = LayerSeverity(sigma, alpha, limit1, excess1);
+    let layerSeverity2 = LayerSeverity(sigma, alpha, limit2, excess2);
+    return layerSeverity2 / layerSeverity1;
+}
 
+/**
+ * Calculates ILF Layer 2 / ILF Layer 1 ratio.
+ * @param {Number} sigma Pareto Type I scale parameter.
+ * @param {Number} alpha Pareto Type I shape parameter.
+ * @param {Number} limit1 Layer 1 Limit (millions).
+ * @param {Number} excess1 Layer 1 Excess FGU (millions).
+ * @param {Number} limit2 Layer 2 Limit (millions).
+ * @param {Number} excess2 Layer 2 Excess FGU (millions).
+ * @returns {Number} Single value (percent).
+ */
+function RateRelativity(sigma, alpha, limit1, excess1, limit2, excess2) {
+    let ilfFactor = IlfFactor(sigma, alpha, limit1, excess1, limit2, excess2);
+    let limitsRatio = limit2 / limit1;
+    return ilfFactor / limitsRatio;
+}
 
+/**
+ * Calculates probability that FGU loss severity is greater than Excess FGU.
+ * @param {Number} sigma Pareto Type I scale parameter.
+ * @param {Number} alpha Pareto Type I shape parameter.
+ * @param {Number} excess Excess FGU (millions).
+ * @returns {Number} Single value (percent).
+ */
+function ExceedenceProbability(sigma, alpha, excess) {
+    return ( sigma / excess ) ** alpha;
+}
+
+/**
+ * Calculates average Layer cost using the Base Rate & ILF method.
+ * @param {Number} sigma Pareto Type I scale parameter.
+ * @param {Number} alpha Pareto Type I shape parameter.
+ * @param {Number} baseRate Ratio of average Base cost to Base Limit (units per million).
+ * @param {Number} baseLimit Base Layer Limit (millions).
+ * @param {Number} baseExcess Base Layer Excess FGU (millions).
+ * @param {Number} limit Layer Limit (millions).
+ * @param {Number} excess Layer Excess FGU (millions).
+* @returns {Number} Array of cost (millions) and cost / limit (units).
+ */
+function BaseRateILFCost(sigma, alpha, baseRate, baseLimit, baseExcess, limit, excess) {
+    let ilfFactor = IlfFactor(sigma, alpha, baseLimit, baseExcess, limit, excess);
+    let cost = ( ( baseRate / 1000000 ) * baseLimit ) * ilfFactor;
+    let rate = (cost * 1000000) / limit;
+    return [cost, rate];
+}
+
+/**
+ * Calculates average Layer cost using the Frequency & Severity method.
+ * @param {Number} sigma Pareto Type I scale parameter.
+ * @param {Number} alpha Pareto Type I shape parameter.
+ * @param {Number} baseCount Count of losses greater than Base Layer Excess FGU (units).
+ * @param {Number} baseExcess Base Layer Excess FGU (millions).
+ * @param {Number} limit Layer Limit (millions).
+ * @param {Number} excess Layer Excess FGU (millions).
+ * @returns {Number} Array of cost (millions) and cost / limit (units).
+ */
+function FrequencySeverityCost(sigma, alpha, baseCount, baseExcess, limit, excess) {
+    epBaseExcess = ExceedenceProbability(sigma, alpha, baseExcess)
+    epExcess = ExceedenceProbability(sigma, alpha, excess)
+    let layerSeverity = LayerSeverity(sigma, alpha, limit, excess);
+    let cost = ( ( epExcess / epBaseExcess ) * baseCount ) * layerSeverity;
+    let rate = (cost * 1000000) / limit;
+    return [cost, rate];
+}
+
+// Calcualtions Functions Tests
+
+let sigma = 1;
+let alpha = 0.8;
+let baseCount = 3;
+let baseRate = 123000;
+let baseLimit = 25;
+let baseExcess = 25;
+let limit = 75;
+let excess = 75;
+
+console.log(CappedSeverity(sigma, alpha, baseExcess));
+console.log(LayerSeverity(sigma, alpha, limit, excess));
+console.log(IlfFactor(sigma, alpha, baseLimit, baseExcess, limit, excess));
+console.log(RateRelativity(sigma, alpha, baseLimit, baseExcess, limit, excess));
+console.log(ExceedenceProbability(sigma, alpha, excess));
+console.log(BaseRateILFCost(sigma, alpha, baseRate, baseLimit, baseExcess, limit, excess));
+console.log(FrequencySeverityCost(sigma, alpha, baseCount, baseExcess, limit, excess));
